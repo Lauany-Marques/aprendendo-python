d7a82632a2ea2ae491817926fee7a353b54e614c
Lauany <lauanymreis@outlook.com>
2023-01-23 15:48:10 -0300

    feat: operacoes matematicas

---
+++ operacoes/operacoesmatematicas.py
@@ -0,0 +1,13 @@
+#operacoes matematicas
+#nas operacoes matematicas nao precisa de aspas
+print(2+2)
+print(2-2)
+print(2/2)
+print(2*2)
+#operacoes exponenciais
+print(2**3)
+"""
+esta ultima operacao e o mesmo que 2 elevado a 3
+"""
+print(10/3)
+print(10%3)
